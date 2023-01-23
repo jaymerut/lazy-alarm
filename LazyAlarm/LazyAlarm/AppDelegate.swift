666b2736dc54123f7e5426ed431f31ef1382446a
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-01-22 18:28:29 -0700

    Create LazyAlarm project

---
+++ LazyAlarm/LazyAlarm/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  LazyAlarm
+//
+//  Created by Jayme Rutkoski on 1/22/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+
+}
+
---
+++ LazyAlarm/LazyAlarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LazyAlarm
+//
+//  Created by Jayme Rutkoski on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
